331f0ce294454336be92457fbdd4c8070d1e787a
Jeffry Johnston <git@kidsquid.com>
2011-12-17 13:28:46 -0700

    Unified license texts and formatting. Added extract_somagic_firmware tool.

--- tools/text2bin/text2bin.c
+++ tools/text2bin/text2bin.c
@@ -8,6 +8,7 @@
  * Copyright 2011 Jon Arne Jørgensen
  *
  * This file is part of somagic_dc60
+ * http://code.google.com/p/easycap-somagic-linux/
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,7 +21,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
  *
  */
 
