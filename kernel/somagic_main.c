75f97428c55552547f52010b13d6d62f1c61d693
Pedro <pimartos@gmail.com>
2012-01-26 16:55:26 -0300

    line 97: Upon firmware download, probe should return 0, not -ENODEV

--- kernel/somagic_main.c
+++ kernel/somagic_main.c
@@ -94,7 +94,7 @@ static int __devinit somagic_usb_probe(struct usb_interface *intf,
 
   if (dev->descriptor.idProduct == SOMAGIC_USB_BOOTLOADER_PRODUCT_ID) {
 		somagic_upload_firmware(dev);
-		return -ENODEV;
+		return (0); //-ENODEV;
 	} else if (dev->descriptor.idProduct != SOMAGIC_USB_PRODUCT_ID) {
 		return -ENODEV;
 	}
